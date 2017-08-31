07123e352033f7f5f16db97fc7d808a56bdf24ee
markmclaren <mark.mclaren@gmail.com>
2017-08-31 21:43:23 +0100

    Added KIS widget example files

---
+++ KIS/data.js
@@ -0,0 +1,47 @@
+getKisData({
+    "ukprn": "10007786",
+    "kiscourseid": "4CIVE007UU-201516",
+    "kismode": 1,
+     
+    "teaching": [
+       {
+            "stage": 1,
+            "scheduled": 41,
+            "independent": 59,
+            "placement": 0
+        },
+        {
+            "stage": 2,
+            "scheduled": 44,
+            "independent": 56,
+            "placement": 0
+        },
+        {
+            "stage": 3,
+            "scheduled": 25,
+            "independent": 68,
+            "placement": 7
+        }
+    ],
+    "assessment": [
+       {
+            "stage": 1,
+            "written": 70,
+            "practical": 0,
+            "coursework": 21
+        },
+        {
+            "stage": 2,
+            "written": 70,
+            "practical": 10,
+            "coursework": 20
+ 
+        },
+        {
+            "stage": 3,
+            "written": 45,
+            "practical": 8,  
+            "coursework": 47
+        }
+    ]
+});
